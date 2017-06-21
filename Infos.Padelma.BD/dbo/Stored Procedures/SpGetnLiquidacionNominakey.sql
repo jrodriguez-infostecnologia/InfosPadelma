3bb67631a61ac0b3781ff978d90c08896071cf73
jeferson rodriguez <jrodriguez@infostecnologia.com>
2017-06-21 16:42:59 -0500

    base padelma

--- Infos.Padelma.BD/dbo/Stored Procedures/SpGetnLiquidacionNominakey.sql
+++
@@ -1,2 +0,0 @@
-﻿
-CREATE PROCEDURE [dbo].[SpGetnLiquidacionNominakey] @empresa int,@año int,@mes int,@noPeriodo int,@tipo varchar(50),@numero varchar(50) AS  select * from nLiquidacionNomina where año = @año and empresa = @empresa and mes = @mes and noPeriodo = @noPeriodo and numero = @numero and tipo = @tipo
