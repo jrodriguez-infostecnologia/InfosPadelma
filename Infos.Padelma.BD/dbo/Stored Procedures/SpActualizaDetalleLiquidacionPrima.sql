e655840c9d43fc9234643ffe486aa79123f0f991
jeferson rodriguez <jrodriguez@infostecnologia.com>
2017-07-17 08:57:10 -0500

    ajustes

--- Infos.Padelma.BD/dbo/Stored Procedures/SpActualizaDetalleLiquidacionPrima.sql
+++ Infos.Padelma.BD/dbo/Stored Procedures/SpActualizaDetalleLiquidacionPrima.sql
@@ -1,27 +1,22 @@
 ﻿CREATE PROCEDURE [dbo].[SpActualizaDetalleLiquidacionPrima]
 	@DataTable as ActualizaDetalleLiquidacionPrimaType READONLY
 AS
-MERGE nLiquidacionPrimaDetalle AS liqPrima
-USING @DataTable AS _source
-ON _source.empresa = liqPrima.empresa
-	AND _source.tipo = liqPrima.tipo
-	AND _source.numero = liqPrima.numero
-	AND _source.tercero = liqPrima.tercero
-WHEN NOT MATCHED BY TARGET
-    THEN 
-	INSERT(empresa,tipo,numero,tercero,añoInicial,añoFinal,periodoInicial,periodoFinal,fechaInicial,fechaFinal,fechaIngreso,basico,valorTransporte,valorPromedio,base,diasPromedio,diasPrimas,contrato) 
-	VALUES(_source.empresa,_source.tipo,_source.numero,_source.tercero,_source.añoInicial,_source.añoFinal,_source.periodoInicial,_source.periodoFinal,_source.fechaInicial,_source.fechaFinal,_source.fechaIngreso,_source.basico,_source.valorTransporte,_source.valorPromedio,_source.base,_source.diasPromedio,_source.diasPrimas,_source.contrato)
-WHEN MATCHED 
-    THEN UPDATE SET 
-	liqPrima.basico = _source.basico,
-	liqPrima.valorTransporte = _source.valorTransporte,
-	liqPrima.valorPromedio = _source.valorPromedio,
-	liqPrima.base = _source.base,
-	liqPrima.diasPromedio = _source.diasPromedio,
-	liqPrima.diasPrimas = _source.diasPrimas,
-	liqPrima.valorPrima = _source.valorPrima
-WHEN NOT MATCHED BY SOURCE
-    THEN DELETE 
-OUTPUT $action, inserted.*, deleted.*;
+
+DECLARE @empresa int, @tipo varchar(50), @numero varchar(50);
+
+SELECT DISTINCT 
+@empresa=empresa, 
+@tipo=tipo, 
+@numero=numero
+FROM @DataTable;
+
+DELETE nLiquidacionNominaDetalle 
+WHERE
+@empresa = empresa
+AND @empresa = tipo
+AND @empresa = numero;
+
+INSERT nLiquidacionPrimaDetalle (empresa,tipo,numero,tercero,añoInicial,añoFinal,periodoInicial,periodoFinal,fechaInicial,fechaFinal,fechaIngreso,basico,valorTransporte,valorPromedio,base,diasPromedio,diasPrimas,contrato) 
+SELECT _source.empresa,_source.tipo,_source.numero,_source.tercero,_source.añoInicial,_source.añoFinal,_source.periodoInicial,_source.periodoFinal,_source.fechaInicial,_source.fechaFinal,_source.fechaIngreso,_source.basico,_source.valorTransporte,_source.valorPromedio,_source.base,_source.diasPromedio,_source.diasPrimas,_source.contrato FROM @DataTable _source;
 
 RETURN 0
